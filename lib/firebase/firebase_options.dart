bdfe4d1c82e94e4868fee708033c034c0580bfbb
vanshradadiya14-coder <vanshradadiya14@gmail.com>
2026-07-12 12:41:50 +0100

    Add iOS Firebase config (GoogleService-Info.plist + firebase_options)

--- lib/firebase/firebase_options.dart
+++ lib/firebase/firebase_options.dart
@@ -1,8 +1,8 @@
 // Firebase configuration for the "voltshire" project.
 //
-// Android values are real (from the Firebase console). iOS/Web reuse the same
-// project; if you later build for those platforms, regenerate this file with
-// `flutterfire configure` to get their correct appIds.
+// Android and iOS values are both real (from the Firebase console). Web is
+// not registered; if you later build for web, run `flutterfire configure`
+// to add a real web app config.
 //
 // ignore_for_file: type=lint
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
@@ -33,8 +33,8 @@ class DefaultFirebaseOptions {
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyCkUxfg0_8J8IARLaXoM3DzbfPx284ZG0E',
-    appId: '1:128975017290:android:1c8d46da504384ede45db3',
+    apiKey: 'AIzaSyADvHAz_ftggpz1NAZvRIfXeG12WDO1U4M',
+    appId: '1:128975017290:ios:ec2975a7f440a733e45db3',
     messagingSenderId: '128975017290',
     projectId: 'voltshire',
     storageBucket: 'voltshire.firebasestorage.app',
